44df00461c1e3e8623c6ad7bfe086443eafab935
odalohodalo <davidodalovic@gmail.com>
2025-03-02 22:16:37 +0100

    add navigation tabs component

---
+++ components/Layout/Navigation/NavigationTabs.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+const NavigationTabs = () => {
+  return (
+    <nav>
+      <Button variant="text">TIRES</Button>
+      <Button variant="text">ACCESSORIES</Button>
+      <Button variant="text">DEALS</Button>
+      <Button variant="text">MEMBERSHIP</Button>
+      <Button variant="text">HELP</Button>
+    </nav>
+  );
+};
+
+export default NavigationTabs;
