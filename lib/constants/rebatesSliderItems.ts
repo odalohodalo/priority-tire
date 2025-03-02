b83b3c4a90e94c2fbc8e8be71f5f49af3f4fc7b1
odalohodalo <davidodalovic@gmail.com>
2025-03-02 23:26:23 +0100

    add assets for rebates slider component and create a constants component with data about each item

---
+++ lib/constants/rebatesSliderItems.ts
@@ -0,0 +1,50 @@
+export const REBATES_SLIDER_ITEMS = [
+  {
+    id: 1,
+    title: "Mickey Thompson Rebates",
+    description: "February 15th - March 31st 2025",
+    image: "/images/rebates/tires1.jpg",
+  },
+  {
+    id: 2,
+    title: "General Tires Rebates",
+    description: "March 1st - April 30th 2025",
+    image: "/images/rebates/tires2.jpg",
+  },
+  {
+    id: 3,
+    title: "Pirelli: Save $200 On Sets of 4",
+    description: "25th January - 31st March 2025",
+    image: "/images/rebates/tires3.jpg",
+  },
+  {
+    id: 4,
+    title: "Hankook: Save $200 On Sets Of 4",
+    description: "25th January - 31st March 2025",
+    image: "/images/rebates/tires4.jpg",
+  },
+  {
+    id: 5,
+    title: "Goodyear: Save $200 On Sets Of 4",
+    description: "25th January - 31st March 2025",
+    image: "/images/rebates/tires5.jpg",
+  },
+  {
+    id: 6,
+    title: "Nexen: Save $200 On Sets Of 4",
+    description: "25th January - 31st March 2025",
+    image: "/images/rebates/tires6.jpg",
+  },
+  {
+    id: 7,
+    title: "Kenda: Save $200 On Sets Of 4",
+    description: "25th January - 31st March 2025",
+    image: "/images/rebates/tires7.jpg",
+  },
+  {
+    id: 8,
+    title: "Cooper: Save $120 On Sets Of 4",
+    description: "25th January - 31st March 2025",
+    image: "/images/rebates/tires8.jpg",
+  },
+];
